9118ea2113ef2fd97b4645fc65bf419c8414c301
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-14 17:43:33 +0100

    Create FeatureCard SwiftUI View

---
+++ OnboardingFlow/Views/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by ITHelpDec on 14/05/2024.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
