a43238555eea5c3c86a16ed38d15325e5ef51c88
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-14 09:22:07 +0100

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by ITHelpDec on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
