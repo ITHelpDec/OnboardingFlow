2522a16d3cf0b32940a06200718b92c077c62103
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-14 17:19:34 +0100

    Embed RoundedRectangle in ZStack

--- OnboardingFlow/Views/WelcomePage.swift
+++ OnboardingFlow/Views/WelcomePage.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 150, height: 150)
-                .foregroundStyle(.tint)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.tint)
+            }
             
             Text("Welcome to MyApp")
                 .font(.title)
