2908aa59da3d552f04528edbc62d856fcaddcd06
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-16 15:50:42 -0400

    Part 1 - Layout and style (#1)
      * Build expressive interfaces by combining stacks, spacers, and padding

---
+++ OnboardingFlow/Views/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  OnboardingFlow
+//
+//  Created by ITHelpDec on 14/05/2024.
+//
+
+import SwiftUI
+
+let gradientColours: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            WelcomePage()
+            FeaturesPage()
+        }
+        .background(Gradient(colors: gradientColours))
+        .tabViewStyle(.page)
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingFlow/Views/FeaturesPage.swift
@@ -0,0 +1,34 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by ITHelpDec on 14/05/2024.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack(spacing: 30){
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+                .padding(.top, 100)
+            
+            FeatureCard(iconName: "quote.bubble", description: "\"I like-a...do..da cha-cha...")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Bruce Almighty")
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+        .frame(maxHeight: .infinity)
+        .background(Gradient(colors: gradientColours))
+        .foregroundStyle(.white)
+}
